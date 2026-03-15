fe456e50717b7390f7f37f8b948092d654ed237e
MARKUZDAO <egororeskin21@gmail.com>
2026-03-16 00:27:28 +0300

    fix: Implement WsAdapter for WebSocket platform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws'; // <--- МЫ ДОБАВИЛИ ЭТУ СТРОКУ
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // --- ИСПОЛЬЗУЕМ НОВУЮ "ТРАНСМИССИЮ" ---
+  app.useWebSocketAdapter(new WsAdapter(app)); 
+  // ------------------------------------
+
+  // Render использует переменную PORT для запуска. 
+  // Локально будет использоваться порт 3000.
+  await app.listen(process.env.PORT || 3000); 
 }
 bootstrap();
--- src/pacifica/pacifica.module.ts
+++ src/pacifica/pacifica.module.ts
@@ -3,6 +3,7 @@ import { PacificaService } from './pacifica.service';
 import { PacificaGateway } from './pacifica.gateway';
 
 @Module({
-  providers: [PacificaService, PacificaGateway]
+  providers: [PacificaGateway, PacificaService],
+  exports: [PacificaService], // Экспортируем сервис, чтобы другие могли его использовать
 })
 export class PacificaModule {}
